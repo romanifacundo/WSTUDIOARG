97283df7a03e61fa0664976aaf10d2232c5ac096
Luchodeveloper <lucianost16@gmail.com>
2024-12-26 15:00:49 -0300

    arreglo de error direccion de root

--- W-Studio-Arg/Program.cs
+++ W-Studio-Arg/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.FileProviders;
 using Portafolio.Servicios;
 using W_Studio_Arg.Servicios;
 
@@ -22,6 +23,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+    RequestPath = ""
+});
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
